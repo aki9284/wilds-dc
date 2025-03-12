4e810229bfc42c9f0cc4b4774eb16f98b4287762
aki9284 <aki9284.translator@gmail.com>
2025-03-12 16:47:31 +0900

    GoogleAnalytics対応

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { MenuBar } from "@/components/navigation/MenuBar";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,20 +17,22 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "かりぴきゅれーたー：狩猟笛ダメージ計算機",
   description: "モンスターハンター ワイルズの狩猟笛について様々な条件でダメージ計算を行うことができます。",
+  keywords: "モンスターハンター, 狩猟笛, ダメージ計算, モンスターハンター ワイルズ, 狩猟笛ダメージ計算機, Monster Hunter Wilds, MHWilds, MHWs"
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <MenuBar />
         <main>{children}</main>
+        <GoogleAnalytics gaId="G-TGLJ3FBHG5" />
       </body>
     </html>
   );
